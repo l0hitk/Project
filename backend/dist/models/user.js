0129147cc00bc03e0a267992b6fa3a22b116f48b
l0hitk <127608339+l0hitk@users.noreply.github.com>
2024-04-09 17:02:38 +0530

    Create user.js

---
+++ backend/dist/models/user.js
@@ -0,0 +1,21 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongodb_1 = require("mongodb");
+const mongoose_1 = __importDefault(require("mongoose"));
+const userSchema = new mongoose_1.default.Schema({
+    _id: mongodb_1.ObjectId,
+    auth0Id: { type: String, required: true },
+    email: {
+        type: String,
+        required: true
+    },
+    name: { type: String },
+    institute: { type: String },
+    city: { type: String },
+    branch: { type: String },
+});
+const User = mongoose_1.default.model("User", userSchema);
+exports.default = User;
