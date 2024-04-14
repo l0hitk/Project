4c57a3209a7ee3037f22c8beb6443fa1bd6f5dc8
l0hitk <127608339+l0hitk@users.noreply.github.com>
2024-04-14 15:14:41 +0530

    Update HomePage.tsx

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,29 +1,30 @@
 import land from "../assets/landing.png";
-import appd from "../assets/appDownload.png";
-
-const HomePage= ()=> {
-    return(
-        <div className="flex flex-col gap-12">
-            <div className="md:px-32 bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 text-center -mt-16">
-                <h1 className="text-5xl font-bold tracking-tight text-orange-600">
-                    Welcome to CaféFlow
-                </h1>
-                <span className="text-xl">A online hub to discover our artisanal coffee, delectable eats</span>
-            </div>
-            <div className="grid md:grid-cols-2 gap-5">
-                <img src={land} alt="" />
-                <div className="flex flex-col items-center justify-center gap-4 text-center">
-                    <span className="font-bold text-3xl tracking-tighter">
-                    With Cafeflow, customers can enjoy faster billing, reduced waiting time, and personalized ordering
-                    </span>
-                    <span>
-                        Download our app
-                    </span>
-                    <img src={appd} alt="" />
-                </div>
-            </div>
+const HomePage = () => {
+  return (
+    <div className="flex flex-col gap-12">
+      <div className="md:px-32 bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 text-center -mt-16">
+        <h1 className="text-5xl font-bold tracking-tight text-orange-600">
+          Welcome to CaféFlow
+        </h1>
+        <span className="text-xl">
+          A online hub to discover our artisanal coffee, delectable eats
+        </span>
+      </div>
+      <div className="grid md:grid-cols-2 gap-5">
+        <img src={land} alt="" />
+        <div className="flex flex-col items-center justify-center gap-4 text-center">
+          <span className="font-bold text-3xl tracking-tighter">
+            Real-time Order Tracking
+          </span>
+          <span>
+            With Cafeflow, experience seamless canteen operations with
+            effortless order management, faster billing, and tailored customer
+            experiences.
+          </span>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
